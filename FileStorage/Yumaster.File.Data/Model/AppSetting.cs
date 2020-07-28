6a214ef756c824a5a5d13d747eb13defff1febe5
yumaster <yumaster@yeah.net>
2020-07-28 17:53:32 +0800

    clear

--- FileStorage/Yumaster.File.Data/Model/AppSetting.cs
+++
@@ -1,7 +0,0 @@
-﻿public partial class AppSetting
-{
-    public string Key { get; set; }
-    public string Value { get; set; }
-    public string beizhu { get; set; }
-    public string sm { get; set; }
-}
